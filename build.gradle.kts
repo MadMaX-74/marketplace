3705ec9c10d7c078d639bfe6a8d51a99c0082ed0
maksim <m.maltsev@dsec.ru>
2024-05-30 22:09:20 +0300

    create new branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") apply false
 }
-
 group = "com.otus.otuskotlin.marketplace"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
