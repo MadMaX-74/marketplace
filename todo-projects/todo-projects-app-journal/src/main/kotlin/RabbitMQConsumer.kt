8347e864ff463177288c7bb3ddea18c2e6532671
maksim <m.maltsev@dsec.ru>
2024-10-15 14:22:06 +0300

    fix write file

--- todo-projects/todo-projects-app-journal/src/main/kotlin/RabbitMQConsumer.kt
+++ todo-projects/todo-projects-app-journal/src/main/kotlin/RabbitMQConsumer.kt
@@ -1,23 +1,23 @@
 package ru.otus.todo.app.journal
 
 import com.rabbitmq.client.ConnectionFactory
-import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import java.nio.charset.StandardCharsets
 
 object RabbitMQConsumer {
     private const val r_host = "localhost"
     private const val queueName = "tasks_queue"
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    @OptIn(DelicateCoroutinesApi::class)
     fun start() {
         val factory = ConnectionFactory().apply {
             host = r_host
         }
 
-        GlobalScope.launch(Dispatchers.IO) {
+        scope.launch {
             val connection = factory.newConnection()
             val channel = connection.createChannel()
             channel.queueDeclare(queueName, false, false, false, null)
@@ -27,9 +27,12 @@ object RabbitMQConsumer {
                 val message = String(delivery.body, StandardCharsets.UTF_8)
                 println("Received message: $message")
 
-                // Обрабатываем каждое сообщение в отдельной корутине, используя диспетчер для работы с I/O
-                launch(Dispatchers.IO) {
-                    MessageProcessor.processMessage(message)
+                scope.launch {
+                    try {
+                        MessageProcessor.processMessage(message)
+                    } catch (e: Exception) {
+                        println("Error processing message: ${e.message}")
+                    }
                 }
             }
 
@@ -37,4 +40,3 @@ object RabbitMQConsumer {
         }
     }
 }
-
